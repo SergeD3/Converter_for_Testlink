286ad3f344f646367941fb9f247a16c8e1547acf
Сергей <34037917+SergeD3@users.noreply.github.com>
2022-04-11 13:43:57 +0300

    minor edit

--- test1.py
+++ test1.py
@@ -4,6 +4,7 @@ import xml.etree.ElementTree as ET
 csvFile = 'smoke.csv'
 xmlFile = 'smoke_new.xml'
 p = ET.Element('testcases')
+ind = '\n'
 
 
 with open(csvFile, 'r', newline='', encoding='utf-8') as rf, open(xmlFile, 'w', newline='', encoding='utf-8') as wf:
@@ -14,10 +15,10 @@ with open(csvFile, 'r', newline='', encoding='utf-8') as rf, open(xmlFile, 'w', 
         subel1 = ET.SubElement(el1, 'summary')
         subel2 = ET.SubElement(el1, 'importance')
 
-        p.text = '\n'
+        p.text = ind
         el1.set('name', text_read)
-        subel1.text = row['notes'] + '\n'
-        subel2.text = "Medium" + '\n'
+        subel1.text = row['notes'] + ind
+        subel2.text = "Medium" + ind
 
 
 ET.ElementTree(p).write(xmlFile, encoding="UTF-8", xml_declaration=True)
