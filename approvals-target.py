9c79582e9f7ea6dd49fafd092f1d10a969433bbe
Сергей <34037917+SergeD3@users.noreply.github.com>
2022-04-14 23:47:27 +0300

    Misha, vse ploho... davai zanovo

--- approvals-target.py
+++ approvals-target.py
@@ -1,12 +1,42 @@
 import csv
 import xml.etree.ElementTree as ET
 
-fName = 'target-library-20212-20213'
-dirName = 'general-regression-actual-versions'
+fName = 'smoke'
+dirName = 'smoke'
 csvFile = 'C:/Users/Серж/Desktop/testcases_CRP/scripts/' + dirName + '/' + fName + '.csv'
 xmlFile = 'C:/Users/Серж/Desktop/testcases_CRP/converted_scripts/' + dirName + '/' + fName + '.xml'
-ind = '\n'
+p = ET.Element('testsuite')
 
 
 with open(csvFile, 'r', newline='', encoding='utf-8') as rf, open(xmlFile, 'w', newline='', encoding='utf-8') as wf:
     reader = csv.DictReader(rf, delimiter=",")
+    for row in reader:
+        numb = row['number']
+        ind = row['indent']
+        txt = row['text']
+        nts = row['notes']
+
+        if ind == 1:
+            el1 = ET.SubElement(p, 'testcase' + ind)
+            subel0 = ET.SubElement(el1, 'externalid')
+            subel1 = ET.SubElement(el1, 'summary')
+            subel2 = ET.SubElement(el1, 'importance')
+
+            el1.set('name', txt)
+            subel0.text = numb
+            subel1.text = nts
+            subel2.text = "Medium"
+            p.set('name', fName)
+        elif ind > 1:
+            el1 = ET.SubElement(p, 'testcase' + ind)
+            subel0 = ET.SubElement(el1, 'externalid')
+            subel1 = ET.SubElement(el1, 'summary')
+            subel2 = ET.SubElement(el1, 'importance')
+
+            el1.set('name', txt)
+            subel0.text = numb
+            subel1.text = nts
+            subel2.text = "Medium"
+            p.set('name', fName)
+
+ET.ElementTree(p).write(xmlFile, encoding="UTF-8", xml_declaration=True)
--- conv_script.py
+++ conv_script.py
@@ -1,26 +1,29 @@
 import csv
 import xml.etree.ElementTree as ET
 
-fName = 'target-library-20212-20213'
-dirName = 'general-regression-actual-versions'
+fName = 'smoke'
+dirName = 'smoke'
 csvFile = 'C:/Users/Серж/Desktop/testcases_CRP/scripts/' + dirName + '/' + fName + '.csv'
 xmlFile = 'C:/Users/Серж/Desktop/testcases_CRP/converted_scripts/' + dirName + '/' + fName + '.xml'
 p = ET.Element('testsuite')
-ind = '\n'
 
 
 with open(csvFile, 'r', newline='', encoding='utf-8') as rf, open(xmlFile, 'w', newline='', encoding='utf-8') as wf:
     reader = csv.DictReader(rf, delimiter=",")
     for row in reader:
-        text_read = row['text']
-        notes_read = row['notes']
-        el1 = ET.SubElement(p, 'testcase' + ind)
+        numb = row['number']
+        ind = row['indent']
+        txt = row['text']
+        nts = row['notes']
+
+        el1 = ET.SubElement(p, 'testcase')
+        subel0 = ET.SubElement(el1, 'externalid')
         subel1 = ET.SubElement(el1, 'summary')
         subel2 = ET.SubElement(el1, 'importance')
 
-        p.text = ind
-        el1.set('name', text_read)
-        subel1.text = row['notes']
+        el1.set('name', txt)
+        subel0.text = numb
+        subel1.text = nts
         subel2.text = "Medium"
         p.set('name', fName)
 
