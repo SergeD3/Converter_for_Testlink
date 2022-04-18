5e5b4ba665263eeb20fe1f4749cd3aca8a9a6370
Сергей <34037917+SergeD3@users.noreply.github.com>
2022-04-18 17:32:54 +0300

    переработал структуру скрипта. Теперь если отступ >1 то он конвертируется как тесткейс с привязкой к тест сьюту. пришлось закомментировать функцию по запоминанию предыдущего значения в итерации цикла.

--- approvals-target.py
+++ approvals-target.py
@@ -3,17 +3,16 @@ import xml.etree.ElementTree as ET
 
 fName = 'smoke'
 dirName = 'smoke'
+re = '\n'
 csvFile = 'C:/Users/Серж/Desktop/testcases_CRP/scripts/' + dirName + '/' + fName + '.csv'
 xmlFile = 'C:/Users/Серж/Desktop/testcases_CRP/converted_scripts/' + dirName + '/' + fName + '.xml'
 p = ET.Element('testsuite')
 pastiter = []
+suite_num = []
 
 
-def lstnmb(x, i):  # функция для сохранения предыдущего значения numb в цикле
-    pastiter.insert(i, x)
-    retpst = pastiter[i-2]
-#    print('Old: ', retpst)
-    return retpst
+# def lstnmb(x, i):   функция для сохранения значения numb == 1 из цикла
+#     pastiter.insert(i, x)
 
 
 with open(csvFile, 'r', newline='', encoding='utf-8') as rf, open(xmlFile, 'w', newline='', encoding='utf-8') as wf:
@@ -25,30 +24,23 @@ with open(csvFile, 'r', newline='', encoding='utf-8') as rf, open(xmlFile, 'w', 
         ind = row['indent']
         txt = row['text']
         nts = row['notes']
-        print(numb)
-        lstnmb(numb, i)
 
-        # if ind == 1:
-        #     el1 = ET.SubElement(p, 'testcase' + ind)
-        #     subel0 = ET.SubElement(el1, 'externalid')
-        #     subel1 = ET.SubElement(el1, 'summary')
-        #     subel2 = ET.SubElement(el1, 'importance')
-        #
-        #     el1.set('name', txt)
-        #     subel0.text = numb
-        #     subel1.text = nts
-        #     subel2.text = "Medium"
-        #     p.set('name', fName)
-        # elif ind > 1:
-        #     el1 = ET.SubElement(p, 'testcase' + ind)
-        #     subel0 = ET.SubElement(el1, 'externalid')
-        #     subel1 = ET.SubElement(el1, 'summary')
-        #     subel2 = ET.SubElement(el1, 'importance')
-        #
-        #     el1.set('name', txt)
-        #     subel0.text = numb
-        #     subel1.text = nts
-        #     subel2.text = "Medium"
-        #     p.set('name', fName)
+        if int(ind) == 1:
+            p.set('name', fName)
+            el1 = ET.SubElement(p, 'testsuite' + re)
+            el1.set('name', txt)
+            el1.text = re
 
-# ET.ElementTree(p).write(xmlFile, encoding="UTF-8", xml_declaration=True)
+        elif int(ind) > 1:
+            el2 = ET.SubElement(el1, 'testcase' + re)
+            el2.set('name', txt)
+            subel0 = ET.SubElement(el2, 'externalid')
+            subel1 = ET.SubElement(el2, 'summary')
+            subel2 = ET.SubElement(el2, 'importance')
+
+            subel0.text = numb
+            subel1.text = nts
+            subel2.text = "Medium"
+
+
+ET.ElementTree(p).write(xmlFile, encoding="UTF-8", xml_declaration=True)
--- conv_script.py
+++ conv_script.py
@@ -3,45 +3,39 @@ import xml.etree.ElementTree as ET
 
 fName = 'smoke'
 dirName = 'smoke'
-i = '\n'
+re = '\n'
 csvFile = 'C:/Users/Серж/Desktop/testcases_CRP/scripts/' + dirName + '/' + fName + '.csv'
 xmlFile = 'C:/Users/Серж/Desktop/testcases_CRP/converted_scripts/' + dirName + '/' + fName + '.xml'
 p = ET.Element('testsuite')
+pastiter = []
+suite_num = []
 
 
 with open(csvFile, 'r', newline='', encoding='utf-8') as rf, open(xmlFile, 'w', newline='', encoding='utf-8') as wf:
     reader = csv.DictReader(rf, delimiter=",")
+    i = 0
     for row in reader:
+        i += 1
         numb = row['number']
         ind = row['indent']
         txt = row['text']
         nts = row['notes']
+
         if int(ind) == 1:
-            el1 = ET.SubElement(p, 'testcase' + i)
-            subel0 = ET.SubElement(el1, 'externalid')
-            subel1 = ET.SubElement(el1, 'summary')
-            subel2 = ET.SubElement(el1, 'importance')
-
-            el1.set('name', txt)
-            subel0.text = numb + i
-            subel1.text = nts + i
-            subel2.text = "Medium" + i
             p.set('name', fName)
-            el1.text = i
+            el1 = ET.SubElement(p, 'testsuite' + re)
+            el1.set('name', txt)
+            el1.text = re
+
         elif int(ind) > 1:
-            p1 = ET.SubElement(p, 'testsuite' + i)
-            el1 = ET.SubElement(p1, 'testcase' + i)
-            subel0 = ET.SubElement(el1, 'externalid')
-            subel1 = ET.SubElement(el1, 'summary')
-            subel2 = ET.SubElement(el1, 'importance')
+            el2 = ET.SubElement(el1, 'testcase' + re)
+            el2.set('name', txt)
+            subel0 = ET.SubElement(el2, 'externalid')
+            subel1 = ET.SubElement(el2, 'summary')
+            subel2 = ET.SubElement(el2, 'importance')
 
-            el1.set('name', txt)
-            subel0.text = numb + i
-            subel1.text = nts + i
-            subel2.text = "Medium" + i
-            p.set('name', fName)
-            p1.set('name', txt)
-            el1.text = i
-
+            subel0.text = numb
+            subel1.text = nts
+            subel2.text = "Medium"
 
 ET.ElementTree(p).write(xmlFile, encoding="UTF-8", xml_declaration=True)
