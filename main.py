3b4e0493667d73b524385511f7893781ee91d7c4
Сергей <34037917+SergeD3@users.noreply.github.com>
2022-04-20 23:10:01 +0300

    Продолжение эпопеи с автоматизацией

--- main.py
+++ main.py
@@ -16,11 +16,11 @@ if __name__ == "__main__":
     csvFile = getpath(full_path)
     filename = getnames(full_path)
 
-    while a < len(filename):
+    while i < len(filename):
+        csvfile1 = csvFile['path'][i]
+        xmlfile1 = str(filename[i])
+        xmlfile = 'C:/Users/Серж/Desktop/testcases_CRP/converted_scripts/' + dirName + '/' + xmlfile1 + '.xml'
         i += 1
-        a += 1
-        csvfile1 = csvFile['path'][i-1]
-        xmlfile = 'C:/Users/Серж/Desktop/testcases_CRP/converted_scripts/' + dirName + '/' + str(filename[i-1]) + '.xml'
         with open(csvfile1, 'r', newline='', encoding='utf-8') as rf, open(xmlfile, 'w', newline='', encoding='utf-8') as wf:
             reader = csv.DictReader(rf, delimiter=",")
             for row in reader:
@@ -29,8 +29,8 @@ if __name__ == "__main__":
                 txt = row['text']
                 nts = row['notes']
 
-                if int(ind) == 1:
-                    p.set('name', filename[i-1])
+                if int(ind) <= 1:
+                    p.set('name', xmlfile1)
                     el1 = ET.SubElement(p, 'testsuite' + re)
                     el1.set('name', txt)
                 #            el1.text = re
@@ -39,9 +39,8 @@ if __name__ == "__main__":
                     el2 = ET.SubElement(el1, 'testcase' + re)
                     el2.set('name', txt)
                     el2.set('internalid', ind)
-                    #           subel0 = ET.SubElement(el2, 'externalid')
+                    subel0 = ET.SubElement(el2, 'externalid')
                     subel1 = ET.SubElement(el2, 'summary')
-                    #            subel0.text = numb
+                    subel0.text = numb
                     subel1.text = nts
-
             ET.ElementTree(p).write(xmlfile, encoding="UTF-8", xml_declaration=True)
