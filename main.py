feaa45151d3cbf0300ff69ed7ed031658b027118
Сергей <34037917+SergeD3@users.noreply.github.com>
2022-04-28 15:31:02 +0300

    получилось автоматизировать процесс. Скормить папку скрипту и он сам его обработает.

--- main.py
+++ main.py
@@ -3,26 +3,24 @@ if __name__ == "__main__":
     import xml.etree.ElementTree as ET
     from poligon import getpath, getnames
 
-    dirName = 'general-regression-actual-versions'
-    full_path = 'C:/Users/Серж/Desktop/testcases_CRP/scripts/' + dirName + '/'
+    dirName = 'completed-new-feature-test-runs'  # name of the directory where the checklists.csv are located
+    full_path = 'C:/Users/Серж/Desktop/testcases_CRP/scripts/' + dirName + '/'  # full path to the
+    # directory with checklists
 
-    i = 0
-    a = 0
-    namelist = []
-    pastiter = []
-    suite_num = []
-    p = ET.Element('testsuite')
-    re = '\n'
+    i = 0  # counter
+    re = '\n'  # delimiter
     csvFile = getpath(full_path)
     filename = getnames(full_path)
 
-    while i < len(filename):
+    while i < len(filename):  # this loop iterates through all the files in the directory
         csvfile1 = csvFile['path'][i]
         xmlfile1 = str(filename[i])
+        p = ET.Element('testsuite')
         xmlfile = 'C:/Users/Серж/Desktop/testcases_CRP/converted_scripts/' + dirName + '/' + xmlfile1 + '.xml'
         with open(csvfile1, 'r', newline='', encoding='utf-8') as rf, open(xmlfile, 'w', newline='', encoding='utf-8') as wf:
             reader = csv.DictReader(rf, delimiter=",")
-            for row in reader:
+
+            for row in reader:  # this loop iterates through the lines in the i file
                 numb = row['number']
                 ind = row['indent']
                 txt = row['text']
@@ -31,15 +29,14 @@ if __name__ == "__main__":
                     p.set('name', xmlfile1)
                     el1 = ET.SubElement(p, 'testsuite' + re)
                     el1.set('name', txt)
-                #            el1.text = re
 
                 elif int(ind) > 1:
                     el2 = ET.SubElement(el1, 'testcase' + re)
                     el2.set('name', txt)
-                    el2.set('internalid', ind)
+#                    el2.set('internalid', ind)
                     subel0 = ET.SubElement(el2, 'externalid')
                     subel1 = ET.SubElement(el2, 'summary')
-                    subel0.text = numb
+#                    subel0.text = numb
                     subel1.text = nts
 
             ET.ElementTree(p).write(xmlfile, encoding="UTF-8", xml_declaration=True)
